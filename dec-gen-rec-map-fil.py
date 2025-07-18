78b6831254f5c278b8733d558ec3c404ceb8cc8f
ramin <ramindesu88@gmail.com>
2025-07-18 16:19:49 +0330

    question 1

---
+++ dec-gen-rec-map-fil.py
@@ -0,0 +1,16 @@
+# def fibonacci(n):
+#     if n == 0:
+#         yield 0
+#         return
+#     elif n == 1:
+#         yield 0
+#         yield 1
+#         return
+#     else:
+#         prev = list(fibonacci(n - 1))
+#         yield from prev
+#         yield prev[-1] + prev[-2]
+# print(list(fibonacci(23)))
+
+        
+        
