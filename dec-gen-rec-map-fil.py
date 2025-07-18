1444f2d2329ba7d2522a1cbb48dec26e90286136
ramin <ramindesu88@gmail.com>
2025-07-18 16:46:06 +0330

    adding the question number 2

--- dec-gen-rec-map-fil.py
+++ dec-gen-rec-map-fil.py
@@ -12,5 +12,36 @@
 #         yield prev[-1] + prev[-2]
 # print(list(fibonacci(23)))
 
-        
-        
+
+# question number 2
+
+import time
+
+
+# def tim_cheaker(func):
+#     def wrapper():
+#         star = time.time()
+#         result = func()
+#         end = time.time()
+#         duretion = end - star
+#         print(duretion)
+#         return result
+
+#     return wrapper
+
+
+# def counter():
+#     count = 1
+#     while True:
+#         yield count
+#         count += 1
+
+# @tim_cheaker
+# def seconds():
+#     start = time.time()
+#     for sec in counter():
+#         print(sec)
+#         if time.time() - start == 5.0:
+#             break
+
+# seconds()
