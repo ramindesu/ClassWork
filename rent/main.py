1fc546b41d4d56db544c3931d5b9f722de341a5f
ramin <ramindesu88@gmail.com>
2025-08-23 21:43:05 +0330

    ADD(system class)

--- rent/main.py
+++ rent/main.py
@@ -2,38 +2,47 @@ import validate as v
 from abc import ABC
 from datetime import datetime
 
+
 class User(ABC):
-    def __init__(self,name,email):
+    def __init__(self, name, email):
         self.name = v.validate_username(name)
         self.emai = v.validate_email(email)
 
+
 class Customer(User):
-    def __init__(self, name, email,phone):
+    def __init__(self, name, email, phone):
         super().__init__(name, email)
         self.phone = v.validate_phone(phone)
 
+
 class Admin(User):
-    def __init__(self, name, email,id):
+    def __init__(self, name, email, id):
         super().__init__(name, email)
         self.id = id
 
+
 class Car:
-    def __init__(self,maker,model,plate_number,color,available =True):
+    def __init__(self, maker, model, plate_number, color, available=True):
         self.maker = maker
         self.model = model
         self.plate = plate_number
         self.color = color
 
+
 class RentCar:
-    def __init__(self,customer,car,Barrow,return_date):
+    def __init__(self, customer, car, Barrow, return_date):
         self.customer = customer
         self.car = car
         self.barrow = Barrow
         self.return_date = return_date
 
 
-        
+class System:
+    def __init__(self):
+        self.rents = []
 
-        
-    
-        
+    def add_rent(self, rent: RentCar):
+        self.rents.append(rent)
+
+    def remove_rent(self, rent: RentCar):
+        self.rents.remove(rent)
