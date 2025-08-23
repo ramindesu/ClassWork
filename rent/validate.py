51321af3b92a480282785a4ece36d751623434cf
ramin <ramindesu88@gmail.com>
2025-08-23 19:07:48 +0330

    ADD(username validation)

--- rent/validate.py
+++ rent/validate.py
@@ -1,9 +1,14 @@
 import re
-
+class ValidationError(Exception):
+    pass
 username_validate = re.compile(r"^[a-zA-Z]{3,20}$")
 password_validate = re.compile(
     r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z1-9]){8,}$"
 )
 phone_validate = re.compile(r"^\+?\d{10,15}$")
 email_validate = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+
+def validate_username(username):
+    if not username_validate.match(username):
+        raise ValidationError("must be letters and upper than 2 charachter")
 
