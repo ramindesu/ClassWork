e1a4731f88733033f69540fc111e07a595fbe4dd
ramin <ramindesu88@gmail.com>
2025-08-28 21:32:44 +0330

    ADD(documnt about argparse)

---
+++ argsparse_practice.py
@@ -0,0 +1,15 @@
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description="This program is a simple argparse example")
+
+# Add arguments
+parser.add_argument("name", help="Enter your name")
+parser.add_argument("--age", type=int, help="Enter your age")
+
+# Parse the arguments
+args = parser.parse_args()
+
+print(f"Hello {args.name}!")
+if args.age:
+    print(f"Your age is: {args.age}")
