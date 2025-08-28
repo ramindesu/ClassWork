eb14d363649b2c65d2b059cfdfbafdf8d1fa9819
ramin <ramindesu88@gmail.com>
2025-08-28 16:05:30 +0330

    ADD(json part c)

--- Rson.py
+++ Rson.py
@@ -15,7 +15,7 @@ import json
 # print(json_translate(personal_dict))
 
 # part B
-# PATH = "/Users/mohammadi/Downloads/BOOTCAMP/projects/CW/data.json"
+PATH = "/Users/mohammadi/Downloads/BOOTCAMP/projects/CW/data.json"
 
 # with open(PATH,"r") as f:
 #     data = json.load(f)
@@ -25,3 +25,36 @@ import json
 
 # part C
 
+import json
+
+PATH = "students.json"
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+    
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "age": self.age,
+            "grade": self.grade
+        }
+
+students = []
+for i in range(3):
+    name = input("Enter name: ")
+    age = input("Enter age: ")
+    grade = input("Enter grade: ")
+    students.append(Student(name, age, grade))
+
+school = {"students": [s.to_dict() for s in students]}
+
+with open(PATH, "w") as f:
+    json.dump(school, f, indent=4)
+    print(f" Student information saved to {PATH}")
+
+with open(PATH, "r") as f:
+    data = json.load(f)
+    print(json.dumps(data, indent=4))
