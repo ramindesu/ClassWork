8186aebe3c90aa259399d6e93a58598afbcd6d49
ramin <ramindesu88@gmail.com>
2025-08-28 15:49:30 +0330

    ADD(json part 2)

--- Rson.py
+++ Rson.py
@@ -1,16 +1,27 @@
-# part A
+# # part A
 import json
-def json_translate(data:dict):
-    output = json.dumps(data)
-    return output
+# def json_translate(data:dict):
+#     output = json.dumps(data)
+#     return output
     
 
-personal_dict = {
-    "name": "Ramin",
-    "age": 21,
-    "skills": ["python", "django", "yapping"],
-    "eamil": "ramindesu88@gmail.com"
-}
+# personal_dict = {
+#     "name": "Ramin",
+#     "age": 21,
+#     "skills": ["python", "django", "yapping"],
+#     "eamil": "ramindesu88@gmail.com"
+# }
 
-print(json_translate(personal_dict))
+# print(json_translate(personal_dict))
+
+# part B
+# PATH = "/Users/mohammadi/Downloads/BOOTCAMP/projects/CW/data.json"
+
+# with open(PATH,"r") as f:
+#     data = json.load(f)
+
+# for stu in data["students"]:
+#     print(stu["name"])
+
+# part C
 
