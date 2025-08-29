cf7359a4ccc4997bfcdca602222deb9db6969d4c
ramin <ramindesu88@gmail.com>
2025-08-29 10:25:54 +0330

    ADD(question api 2 part 1)

--- apis.py
+++ apis.py
@@ -1,3 +1,6 @@
+import requests
+import os
+os.system("clear")
 # imagine u go to a resturant and order somthing from the menu the waiter(api) sends ur request to the kitchen(back-end) and it gives ur food(ur respond)
 # so yeah it a way that machines talk to eachother 
 # EXAMPLE:
@@ -6,3 +9,28 @@
 
 # --------------------------------------------------
 # PART B
+# ---------------------------------------------------
+# part C
+# -------------------------------------------------------
+
+# QUESTION 2 PART 1
+def status_checker(url):
+    try:
+        url_get = requests.get(url)
+        url_status = url_get.status_code
+        if 200 <= url_status < 300:
+            print("Successful")
+        elif 300 <= url_status < 400:
+            print("Redirection")
+        elif 400 <= url_status < 500:
+            print("Bad request")
+        elif 500 <= url_status < 600:
+            print("Server error")
+        else:
+            print(url_status)
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+status_checker("https://www.w3schools.com/tags/ref_httpmessages.asp")
+status_checker("https://www.w3schools.com/tags/ref_httpmessages.asp/qweqweq")
+
