026c4261a0133a011b8e39e393d1617973c45268
ramin <ramindesu88@gmail.com>
2025-09-05 15:57:10 +0330

    ADD(part c)

--- argsparse_practice.py
+++ argsparse_practice.py
@@ -18,21 +18,19 @@
 # with open("required.json","w") as f:
 #     json.dump(required,f,indent=4)
     
-# # part b 
+# # # part b 
 # import argparse
 # import requests
 # import json
-
+# from pprint import pprint
 # url = 'https://jsonplaceholder.typicode.com/posts'
 # response = requests.get(url)
 # all_posts = response.json()
-
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument("-p", "--user", type=int, help="Filter by userId")
 # parser.add_argument("-f", "--format", default="json", help="Output format (only json supported)")
 # args = parser.parse_args()
-
 
 # posts = all_posts
 # if args.user:
@@ -50,3 +48,34 @@
 
 # print("Data saved successfully to output.json")
 
+# # PART c 
+
+import requests
+from pprint import pprint
+
+class PostFetcher:
+    def __init__(self, api="https://jsonplaceholder.typicode.com/posts"):
+        self.__api = api
+
+    @property
+    def posts(self):
+        response = requests.get(self.__api)
+        posts = response.json()
+        return posts
+
+    def fetch_posts(self):
+        required = []
+        for post in self.posts:
+            required.append({
+                "user_id": post["userId"],
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"][:50]
+            })
+        pprint(required)  
+
+    def filter_posts(self, userId):
+        filtered = [post for post in self.posts if post["userId"] == userId]
+        pprint(filtered) 
+    
+        
