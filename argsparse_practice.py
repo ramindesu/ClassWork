ff3e2cfc2158ad60de03810cff2b492cdee23676
ramin <ramindesu88@gmail.com>
2025-09-05 15:40:23 +0330

    ADD(part ab)

--- argsparse_practice.py
+++ argsparse_practice.py
@@ -1,32 +1,52 @@
-import argparse
-import requests
-import json
-# # Create the parser
-# parser = argparse.ArgumentParser(description="This program is a simple argparse example")
 
-# # Add arguments
-# parser.add_argument("name", help="Enter your name")
-# parser.add_argument("--age", type=int, help="Enter your age")
+# import argparse
+# import requests
+# import json
 
-# # Parse the arguments
+# url = 'https://jsonplaceholder.typicode.com/posts'
+# response = requests.get(url)
+# all_posts = response.json()
+# required = []
+# for post in all_posts:
+#     required.append({
+#         "user_id" : post["userId"],
+#         "id": post["id"],
+#         "title": post["title"],
+#         "body": post["body"][:50]
+#     })
+
+# with open("required.json","w") as f:
+#     json.dump(required,f,indent=4)
+    
+# # part b 
+# import argparse
+# import requests
+# import json
+
+# url = 'https://jsonplaceholder.typicode.com/posts'
+# response = requests.get(url)
+# all_posts = response.json()
+
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-p", "--user", type=int, help="Filter by userId")
+# parser.add_argument("-f", "--format", default="json", help="Output format (only json supported)")
 # args = parser.parse_args()
 
-# print(f"Hello {args.name}!")
-# if args.age:
-#     print(f"Your age is: {args.age}")
-# part A 
-url = 'https://jsonplaceholder.typicode.com/posts'
-response = requests.get(url)
-all_posts = response.json()
-required = []
-for post in all_posts:
-    required.append({
-        "user_id" : post["userId"],
-        "id": post["id"],
-        "title": post["title"],
-        "body": post["body"][:50]
-    })
 
-with open("required.json","w") as f:
-    json.dump(required,f,indent=4)
-    
+# posts = all_posts
+# if args.user:
+#     posts = [post for post in all_posts if post["userId"] == args.user]
+
+
+# parsed_posts = [
+#     {"userId": p["userId"], "id": p["id"], "title": p["title"], "body": p["body"][:50]}
+#     for p in posts
+# ]
+
+
+# with open("output.json", "w") as f:
+#     json.dump(parsed_posts, f, indent=4)
+
+# print("Data saved successfully to output.json")
+
