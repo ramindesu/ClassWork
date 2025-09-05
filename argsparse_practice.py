8db9d4f04fde617197b3164937525c62c54d882c
ramin <ramindesu88@gmail.com>
2025-09-05 15:09:45 +0330

    ADD(part a)

--- argsparse_practice.py
+++ argsparse_practice.py
@@ -1,5 +1,6 @@
 import argparse
-
+import requests
+import json
 # # Create the parser
 # parser = argparse.ArgumentParser(description="This program is a simple argparse example")
 
@@ -13,4 +14,19 @@ import argparse
 # print(f"Hello {args.name}!")
 # if args.age:
 #     print(f"Your age is: {args.age}")
+# part A 
+url = 'https://jsonplaceholder.typicode.com/posts'
+response = requests.get(url)
+all_posts = response.json()
+required = []
+for post in all_posts:
+    required.append({
+        "user_id" : post["userId"],
+        "id": post["id"],
+        "title": post["title"],
+        "body": post["body"][:50]
+    })
 
+with open("required.json","w") as f:
+    json.dump(required,f,indent=4)
+    
