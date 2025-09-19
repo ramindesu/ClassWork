fcb486e1fd1b12728022b9fa27c5b96440a1d6da
ramin <ramindesu88@gmail.com>
2025-09-19 15:09:30 +0330

    question 1

---
+++ frond-end/main.js
@@ -0,0 +1,39 @@
+// let number_1 = +prompt("Please enter your first number");
+// let operator = prompt("Also select an operator (-, +, *, /)");
+// let number_2 = +prompt("Enter the second number");
+
+
+
+
+// function calculate(number_1, operator, number_2) {
+//   if (operator == "+") {
+//     add(number_1, number_2);
+//   } else if (operator == "-") {
+//     sub(number_1, number_2);
+//   } else if (operator == "*") {
+//     multiply(number_1, number_2);
+//   } else if (operator == "/") {
+//     division(number_1, number_2);
+//   } else {
+//     console.log("Invalid operator!");
+//   }
+// }
+
+// function add(number_1, number_2) {
+//   console.log(number_1 + number_2);
+// }
+// function sub(number_1, number_2) {
+//   console.log(number_1 - number_2);
+// }
+// function multiply(number_1, number_2) {
+//   console.log(number_1 * number_2);
+// }
+// function division(number_1, number_2) {
+//   if (number_2 == 0) {
+//     console.log("Can't divide by zero");
+//   } else {
+//     console.log(number_1 / number_2);
+//   }
+// }
+
+// calculate(number_1, operator,number_2);
