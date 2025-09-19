398f9fd24e481adf1a7a7b1401a38f8f050267ac
ramin <ramindesu88@gmail.com>
2025-09-19 15:17:30 +0330

    question 2

--- frond-end/main.js
+++ frond-end/main.js
@@ -2,9 +2,6 @@
 // let operator = prompt("Also select an operator (-, +, *, /)");
 // let number_2 = +prompt("Enter the second number");
 
-
-
-
 // function calculate(number_1, operator, number_2) {
 //   if (operator == "+") {
 //     add(number_1, number_2);
@@ -37,3 +34,15 @@
 // }
 
 // calculate(number_1, operator,number_2);
+
+// question number 2
+let numbers = +prompt("please enter a number");
+let sum = 0;
+if (numbers < 1) {
+  console.log("u need a postive number");
+} else {
+  for (let i = 0; i < numbers; i++) {
+    sum += i
+  }
+  console.log(sum)
+}
