e477ad1624a806133c870a293c7735e475767b57
ramin <ramindesu88@gmail.com>
2025-10-03 15:06:18 +0330

    ADD(use tasks)

--- todo/main.py
+++ todo/main.py
@@ -7,3 +7,21 @@ service = TodoService(data)
 
 
 service.create_tables()
+
+todo_id = service.add_todo(
+    user_id=1,
+    title="Learn psycopg",
+    description="Practice DB with Python",
+    due_time="2025-10-05 12:00:00"
+)
+print("Todo created:", todo_id)
+
+
+todos = service.list_todos(1)
+print("Todos:", todos)
+
+
+service.update_todo_status(1, todo_id, "done")
+
+
+service.delete_todo(1, todo_id)
