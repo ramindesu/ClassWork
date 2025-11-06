dbccde6d6f43da7d2b8a7a9fe7ae0534498e980b
ramin <ramindesu88@gmail.com>
2025-11-06 15:07:09 +0330

    FIX(register)

--- Pet_store/Shelter/admin.py
+++ Pet_store/Shelter/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from .models import Pet, Owner
 
+
 @admin.register(Pet)
-class AdminPet(admin.ModelAdmin):
-    list_display = ('name', 'species', 'age', 'availability')
-    list_filter = ('species', 'availability')
-    search_fields = ('name', 'species')
-    ordering = ('name',)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ("name", "species", "age", "availability")
+    list_filter = ("species", "availability")  
+    search_fields = ("name", "species") 
+
 
 @admin.register(Owner)
-class AdminOwner(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'pet', 'request_time')
-    search_fields = ('first_name', 'last_name')
-    list_filter = ('request_time',)
-    ordering = ('first_name',)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "pet", "request_time")
+    search_fields = ("first_name", "last_name")
