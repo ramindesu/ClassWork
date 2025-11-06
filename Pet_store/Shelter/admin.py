d022b44383643888eb58d49202fed2cb0ee9c581
ramin <ramindesu88@gmail.com>
2025-11-06 15:04:42 +0330

    ADD(registration to admin)

--- Pet_store/Shelter/admin.py
+++ Pet_store/Shelter/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Pet, Owner
 
-# Register your models here.
+@admin.register(Pet)
+class AdminPet(admin.ModelAdmin):
+    list_display = ('name', 'species', 'age', 'availability')
+    list_filter = ('species', 'availability')
+    search_fields = ('name', 'species')
+    ordering = ('name',)
+
+@admin.register(Owner)
+class AdminOwner(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'pet', 'request_time')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('request_time',)
+    ordering = ('first_name',)
